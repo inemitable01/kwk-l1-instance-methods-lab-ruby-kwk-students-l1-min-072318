d5296d9263ccfbee91589a5df82d0e1c011e889a
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 16:00:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# dog.rb
+class Dog
+  def bark
+    puts "Woof!"
+  end 
+def sit 
+  puts "The Dog is sitting"
+     end
+  class Person
+    def talk
+      puts "Hello World!"
+    end 
+    class Person
+    def talk
+      puts "Hello World!"
+ 
+      puts "The Person is walking."
+    end 
+  end 

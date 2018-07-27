6284b8c769a5d03d35943d2a7a66bb1577cf59a4
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 16:46:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,8 @@ class Dog
   def bark
     puts "Woof!"
   end 
-def sit 
-  puts "The Dog is sitting"
-     end
-  class Person
-    def talk
-      puts "Hello World!"
-    end 
-    class Person
-    def talk
-      puts "Hello World!"
- 
-      puts "The Person is walking."
-    end 
-  end 
+  def sit 
+    puts "The Dog is sitting"
+  end
+end 
+  

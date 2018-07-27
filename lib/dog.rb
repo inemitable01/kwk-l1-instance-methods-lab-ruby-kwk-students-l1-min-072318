56f093c384925935eab37c75d2782ec897f4082e
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-27 16:40:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,15 +4,6 @@ class Dog
   end 
 def sit 
   puts "The Dog is sitting"
-     end
-  class Person
-    def talk
-      puts "Hello World!"
-    end 
-    class Person
-    def talk
-      puts "Hello World!"
- 
-      puts "The Person is walking."
-    end 
-  end 
+  end
+end 
+  
